ea064f083b9a4c47c7bc6204bc0826c08bb8c331
Quinn Weaver <quinn@pgexperts.com>
2018-10-14 23:20:33 -0700

    Add table in different schema, to test --exclude-table-in-database

--- fixture.sh
+++ fixture.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
 DB=ff
-
 dropdb "$DB"
 createdb "$DB"
 export PGDATABASE="$DB"
 
 N_ROWS=10000000
-psql -c "drop table nine_tenths_bloat"
-psql -c "create table nine_tenths_bloat as select * from generate_series(1, $N_ROWS) as id"
-psql -c "delete from nine_tenths_bloat where id % 10 <> 1"
-psql -c "analyze nine_tenths_bloat"
+
+p() {
+    psql --no-psqlrc --echo-queries --command "$@"
+}
+
+
+p "create table if not exists nine_tenths_bloat as select * from generate_series(1, $N_ROWS) as id"
+p "delete from nine_tenths_bloat where id % 10 <> 1"
+p "analyze nine_tenths_bloat"
+
+
+p "create schema if not exists ff_schema"
+p "create table if not exists ff_schema.eight_tenths_bloat as select * from generate_series(1, $N_ROWS) as id"
+p "delete from ff_schema.eight_tenths_bloat where id % 10 not in (1, 2)"
+p "analyze ff_schema.eight_tenths_bloat"
+
+
